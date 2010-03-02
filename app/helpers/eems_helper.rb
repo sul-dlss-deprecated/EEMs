8ff82b49b444115cf7c626ad2ddfa6f79116d726
Willy Mene <wmene@stanford.edu>
2010-03-02 13:48:56 -0800

    Created eems_text_field_helper

--- app/helpers/eems_helper.rb
+++ app/helpers/eems_helper.rb
@@ -1,3 +1,5 @@
 module EemsHelper
-  
+  def eems_text_field_tag(prop)
+    text_field_tag("eem[#{prop.to_s}]")
+  end
 end
---
+++ spec/helpers/eems_helper_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe EemsHelper do
+  
+  describe "#eems_text_field_tag" do
+    it "should generate the html for an eem property using the field name and text" do
+      html =<<-EOF
+      <p>
+          <%= text_field_tag 'eem[note]' %>
+          <input id="eem_note" name="eem[note]" type="text" />
+      </p>
+      EOF
+      
+      out = helper.eems_text_field_tag(:note)
+      out.should =~ /<input id=\"eem_note\" name=\"eem\[note\]\" type=\"text\" \/>/
+      
+    end
+  end
+end
