284fe7469b81e2f88757f6161d815367526ab142
Chris Jesudurai <jchris@administrators-macbook.local>
2010-04-13 09:51:21 -0700

    Modified blacklight detail page based on the mocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,20 @@ module ApplicationHelper
   def application_name
     return 'EEMs'
   end
+
   def render_document_heading
     '<h1>' + document_heading.to_s + '</h1>'
   end
+  
+  # Get value for a given eem field
+  def print_solr_field(name, msg = '') 
+    value = eval("@document[:#{name.to_s}]")
+    
+    if (value.nil? || value.empty?)
+      value = msg
+    end
+    
+    return value
+  end 
+  
 end
