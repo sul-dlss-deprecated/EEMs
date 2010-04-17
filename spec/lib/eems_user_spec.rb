510fac86438a1d7a2d861a403de4a9855f8f0114
Willy Mene <wmene@stanford.edu>
2010-04-17 00:02:22 -0700

    Hidden fields for sunetid and usernameEemsUser#display_name_lf.

--- spec/lib/eems_user_spec.rb
+++ spec/lib/eems_user_spec.rb
@@ -1,9 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe EemsUser do
-  it "should should generate a display name" do
+  it "should generate a display name" do
     u = EemsUser.new('first', 'last', 'sunetid')
     u.display_name.should == 'first last'
+  end
+  
+  it "should generate a last, first display name" do
+    u = EemsUser.new('first', 'last', 'sunetid')
+    u.display_name_lf.should == 'last, first'
   end
   
   describe "#find" do
