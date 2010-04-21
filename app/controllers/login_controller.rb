29b60f0206f35935b6aeb947e8343eb38ed5b69b
Lyberteam Administration <lyberadmin@lyberapps-dev.stanford.edu>
2010-04-21 16:57:07 -0700

    Changed session[:referrer] to params[:referrer] in login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,12 +2,12 @@
 class LoginController < ApplicationController
 
   def new
-    session[:referrer] = params[:referrer]
   end
   
   #This method protected by webauth as configured in apache
   #We should only get here after a user has succesfully authenticated
   def webauth
-    redirect_to session[:referrer]
+    Rails.logger.info("Session user_id is: " << session[:user_id])
+    redirect_to params[:referrer]
   end
 end
