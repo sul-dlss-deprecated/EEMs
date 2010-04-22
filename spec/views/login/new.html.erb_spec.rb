b6ae19a98047cc87de3a29eabad308ac0e04ad92
Willy Mene <wmene@stanford.edu>
2010-04-22 12:19:31 -0700

    specs now pass after change to use referrer param

--- spec/views/login/new.html.erb_spec.rb
+++ spec/views/login/new.html.erb_spec.rb
@@ -2,8 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe " New Login view" do
   it "should display a link to /login/webauth" do
+    params[:referrer] = 'http://cnn.com'
     render "login/new.html.erb"
     
-    response.body.should =~ /<a href=\"\/login\/webauth\">/
+    response.body.should =~ /href=\'\/login\/webauth\/\?referrer/
   end
 end
