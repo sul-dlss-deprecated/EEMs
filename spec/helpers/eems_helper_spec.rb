dc026e0193c82bd279e9bf64b2d38b78989db2ce
Christopher Jesudurai <jchris@Christopher-Jesudurais-Mac-mini.local>
2010-05-12 03:07:13 -0700

    Added couple of helper rspec tests

--- spec/helpers/eems_helper_spec.rb
+++ spec/helpers/eems_helper_spec.rb
@@ -15,5 +15,19 @@ describe EemsHelper do
       out.should =~ /<input id=\"eem_note\" name=\"eem\[note\]\" type=\"text\" value=\"\" \/>/      
     end
   end
+  
+  describe "#get_source_url" do
+    it "should return a URL or an empty string for a given referrer object" do
+      helper.get_source_url('https://jirasul.stanford.edu/jira/browse/EEMS-13').should == 'https://jirasul.stanford.edu/jira/browse/EEMS-13'
+      helper.get_source_url(nil).should == ""      
+    end 
+  end  
+
+  describe "#shorten_url" do
+    it "should return a shortened URL (if length is less than 50 chars) for a given URL" do
+      helper.shorten_url('http://www.irs.gov/app/picklist/list/formsPublications.html').should == 'http://www.irs.gov/app/picklist/list/formsPublicat...'
+      helper.shorten_url('http://www.stanford.edu/01.pdf').should == 'http://www.stanford.edu/01.pdf'      
+    end 
+  end  
     
 end
