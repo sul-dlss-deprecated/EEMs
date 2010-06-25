4096c42bc6549bce234290f8392b3b1e3349065b
Willy Mene <wmene@stanford.edu>
2010-06-25 16:38:16 -0700

    Create an Eem without PDF url.  Jira EEMS-111

---
+++ spec/controllers/eems_controller_no_pdf_spec.rb
@@ -0,0 +1,66 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'json'
+
+describe EemsController do
+  before(:all) do
+    ActiveFedora::SolrService.register(SOLR_URL)
+    Fedora::Repository.register(FEDORA_URL)
+    Fedora::Repository.stub!(:instance).and_return(stub('frepo').as_null_object)
+  end
+  
+  it "should be restful" do
+    params_from(:post, "/eems/no_pdf").should == {:controller => 'eems', :action => 'no_pdf'}
+  end
+  
+  describe "#no_pdf" do
+    before(:each) do
+      @eems_params = HashWithIndifferentAccess.new(
+        {
+          :copyrightStatusDate => '1/1/10',
+          :copyrightStatus => 'pending',
+          :creatorName => 'Joe Bob',
+          :creatorType => 'person',
+          :language => 'English',
+          :note => 'text of note',
+          :paymentType => 'free|paid',
+          :paymentFund => 'BIOLOGY',
+          :selectorName => 'Bob Smith',
+          :selectorSunetid => 'bsmith',
+          :title => 'title',
+          :sourceUrl => 'http://something.org/papers',
+        }
+      )
+      
+      @eem = Eem.new(:pid => 'pid:123')
+      @eem.set_properties(@eems_params.stringify_keys)
+      @eem.should_receive(:save).twice
+
+      Eem.should_receive(:from_params).with(@eems_params).and_return(@eem)
+      
+      @eem.should_receive(:add_datastream).with(an_instance_of(Dor::ActionLogDatastream))
+
+      
+      session[:user_id] = 'somesunetid'
+      EemsUser.stub!(:valid?).with('somesunetid').and_return(true)
+      post "no_pdf", :eem => @eems_params
+      
+    end
+    
+    it "should create a new Eem from the params hash" do
+     
+    end
+        
+    it "should return json with just the eem pid" do
+      json = JSON.parse(response.body)
+      json.should == {
+        'eem_pid' => 'pid:123',
+        }
+    end
+    
+    it "should create an ActionLog datastream" do
+      
+    end
+    
+  end
+  
+end
