9c23a4e66dd808c5bd10c3d138e1a8049613f323
Willy Mene <wmene@stanford.edu>
2010-07-02 17:35:43 -0700

    Added attempts column to ContentFile.  We stop download after 4 attempts.  Jira EEMS-112.  ContentFileController now sends back attempts in json response

--- spec/lib/download_job_spec.rb
+++ spec/lib/download_job_spec.rb
@@ -1,21 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'curl'
 require 'active_fedora'
+require 'tempfile'
 
 describe Dor::DownloadJob do
   before(:all) do
     ActiveFedora::SolrService.register(SOLR_URL)
     Fedora::Repository.register(FEDORA_URL)
     Fedora::Repository.stub!(:instance).and_return(stub('frepo').as_null_object)
-    FileUtils.mkdir(File.join(Sulair::WORKSPACE_DIR, 'druid:123')) unless (File.exists?(File.join(Sulair::WORKSPACE_DIR, 'druid:123')))
+    
   end
-  
-  after(:all) do
-    FileUtils.rm_rf(File.join(Sulair::WORKSPACE_DIR, 'druid:123'))
-  end
-  
+    
   describe "perform" do
     it "should download the content file using Curl and update progress percentage and set the Part as download_done" do
+      FileUtils.mkdir(File.join(Sulair::WORKSPACE_DIR, 'druid:123')) unless (File.exists?(File.join(Sulair::WORKSPACE_DIR, 'druid:123')))
+      
       cf = ContentFile.new
       cf.url = 'http://stanford.edu/images/stanford_title.jpg'
       cf.filepath = File.join(Sulair::WORKSPACE_DIR, 'druid:123')
@@ -39,6 +38,40 @@ describe Dor::DownloadJob do
       part.datastreams['properties'].filename_values.should == ['stanford_title.jpg']
       
       File.file?(File.join(cf.filepath, 'stanford_title.jpg')).should be_true
+      
+      FileUtils.rm_rf(File.join(Sulair::WORKSPACE_DIR, 'druid:123'))
+    end
+  end
+  
+  describe "error handling" do
+    it "should increment the number of attempts if an exception is thrown" do
+      
+      cf = ContentFile.new
+      cf.url = 'http://stanford.edu/images/stanford_title.jpg'
+      cf.attempts = 1
+            
+      ContentFile.stub!(:find).and_return(cf)
+      Tempfile.should_receive(:new).and_raise(Exception.new)
+      job = Dor::DownloadJob.new(1)
+      cf.should_receive(:attempts=).with(2)
+      cf.should_receive(:save)
+      lambda{ job.perform }.should raise_exception
+            
+    end
+    
+    it "should not set attempts to greater than 5" do
+      
+      cf = ContentFile.new
+      cf.url = 'http://stanford.edu/images/stanford_title.jpg'
+      cf.attempts = 5
+            
+      ContentFile.stub!(:find).and_return(cf)
+      Tempfile.should_receive(:new).and_raise(Exception.new)
+      job = Dor::DownloadJob.new(1)
+      cf.should_not_receive(:attempts=)
+      cf.should_not_receive(:save)
+      lambda{ job.perform }.should raise_exception
+            
     end
   end
 end
