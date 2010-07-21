82cce70febda5de16823de251963f8ab9d047960
Willy Mene <wmene@stanford.edu>
2010-07-20 17:40:52 -0700

    send_to_tech_services now saves at the object level for solr indexing. Jira EEMS-85

--- app/controllers/submit_to_tech_services_controller.rb
+++ app/controllers/submit_to_tech_services_controller.rb
@@ -10,12 +10,12 @@ class SubmitToTechServicesController < ApplicationController
     now = Time.new.xmlschema
     @props_ds.statusDatetime_values = [now]
     @props_ds.requestDatetime_values = [now]
-    @props_ds.save
-    
+
     user = EemsUser.find(session[:user_id])
     action_log = @eem.datastreams['actionLog']
     action_log.log("Request submitted by #{user.display_name}", params[:comment])
-    action_log.save
+
+    @eem.save
     
     Dor::WorkflowService.update_workflow_status('dor', params[:eems_id], 'eemsAccessionWF', 'submit-tech-services', 'completed')
     
