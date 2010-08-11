cd589018ea6ba6836db11aff63efa4e835e80402
Christopher Jesudurai <jchris@stanford.edu>
2010-08-11 13:57:38 -0700

    Fixed EEMS-28, EEMS-85, EEMS-94, EEMS-95 and EEMS-127

--- public/javascripts/eems_detail_actions.js
+++ public/javascripts/eems_detail_actions.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
+
+$(document).ready(function() {	
 	acquisitionsActionsBindToggle('send_to_tech_services');	 
-	acquisitionsActionsBindToggle('delete_this_selection');
+	acquisitionsActionsBindToggle('cancel_this_request');
 	acquisitionsActionsBindToggle('comment_to_selector');	 
+	
+	$('#text_comment_to_selector').val('');
+	$('#text_cancel_this_request').val('');
+
+	// cancel this request
+  $('#cancel_this_request_ok').click(function() {
+    cancelThisRequest();
+  });
+  
+	// question/comment to selector
+  $('#comment_to_selector_ok').click(function() {
+    questionOrCommentToSelector();
+  });
+
+  $('#text_comment_to_selector').keyup(function() { 
+	  var txt = $('#text_comment_to_selector').val();
+
+	  if (/\S/.test(txt)) {
+		  $('#comment_to_selector_ok').removeAttr('disabled');
+	  }	else {
+		  $('#comment_to_selector_ok').attr('disabled', 'disabled');		
+	  }
+	});  
 });
 
 function acquisitionsActionsBindToggle(name) { 
@@ -18,4 +42,35 @@ function acquisitionsActionsBindToggle(name) {
 		);		
 	}
 }
+
+function cancelThisRequest() {
+  var cancelComment = unescapeTags($('#text_cancel_this_request').val());
+  var pid = window._pid;
+  var pars = { 'eem[status]': 'Canceled', 'authenticity_token': window._token };
+
+  eemUpdate(pid, pars);
+
+  var pars = { 
+	  'entry': 'Request canceled by ' + selectorName, 
+	  'comment': cancelComment,
+	  'authenticity_token': window._token
+	};
+	
+  addLogEntry(pid, pars, true);	
+}
+
+function questionOrCommentToSelector() {
+  var questionOrComment = unescapeTags($('#text_comment_to_selector').val());
+
+  if (/\S/.test(questionOrComment)) { 
+	  var pars = { 
+		  'entry': 'Question/comment by ' + selectorName, 
+		  'comment': questionOrComment,
+		  'authenticity_token': window._token
+		};
+		
+    addLogEntry(window._pid, pars, true);	
+	}
+}
+
 
