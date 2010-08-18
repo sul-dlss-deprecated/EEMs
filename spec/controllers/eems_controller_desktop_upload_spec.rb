b7865bfe0a0654d22c8c4d1fc2d44525da46d6ba
Willy Mene <wmene@stanford.edu>
2010-08-18 16:43:35 -0700

    First pass at desktop upload.   All tests pass EEMS-52

---
+++ spec/controllers/eems_controller_desktop_upload_spec.rb
@@ -0,0 +1,69 @@
+
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'json'
+
+describe EemsController do
+  before(:all) do
+    ActiveFedora::SolrService.register(SOLR_URL)
+    Fedora::Repository.register(FEDORA_URL)
+    Fedora::Repository.stub!(:instance).and_return(stub('frepo').as_null_object)
+  end
+  
+  describe "#create" do
+    before(:each) do
+      @file = Tempfile.new(['pre', '.pdf'])
+      @file.stub!(:original_filename).and_return(@file.path.split(/\//).last)
+      @eems_params = HashWithIndifferentAccess.new(
+        {
+          :copyrightStatusDate => '1/1/10',
+          :copyrightStatus => 'pending',
+          :creatorName => 'Joe Bob',
+          :creatorType => 'person',
+          :language => 'English',
+          :note => 'text of note',
+          :paymentType => 'free|paid',
+          :paymentFund => 'BIOLOGY',
+          :selectorName => 'Bob Smith',
+          :selectorSunetid => 'bsmith',
+          :title => 'title',
+          :sourceUrl => 'http://something.org/papers'
+        }
+      )
+      
+      @eem = Eem.new(:pid => 'pid:123')
+      @eem.set_properties(@eems_params.stringify_keys)
+      @eem.should_receive(:save).twice
+      
+      Eem.should_receive(:from_params).with(@eems_params).and_return(@eem)
+      Dor::WorkflowService.should_receive(:create_workflow).with('dor', 'pid:123', 'eemsAccessionWF', ACCESSION_WF_XML)
+      
+      @eem.should_receive(:add_datastream).with(an_instance_of(Dor::ActionLogDatastream))
+      
+      @part = Part.new(:pid => 'part:345')
+      @part.stub!(:save)
+      
+      Part.should_receive(:new).and_return(@part)
+      
+      
+      session[:user_id] = 'somesunetid'
+      EemsUser.stub!(:valid?).with('somesunetid').and_return(true)
+      post "create", :eem => @eems_params, :content_upload => @file
+    end
+    
+    it "should create the content datastream and save it" do    
+      content_ds = @part.datastreams['content']
+      content_ds[:dsLocation].match(/#{Sulair::WORKSPACE_URL}\/pid:123\/pre.pdf/).should_not be_nil
+      
+      props_ds = @part.datastreams['properties']
+      props_ds.filename_values.first.should =~ /pre.pdf/
+      
+      json = JSON.parse(response.body)
+      json.should == {
+        'eem_pid' => 'pid:123',
+        'part_pid'=> 'part:345'
+        }
+    end
+    
+  end
+  
+end
