033f0371d25ffd8e460a05f1419b399ab6501150
Willy Mene <wmene@stanford.edu>
2010-08-30 10:42:54 -0700

    Use eem_model gem as basis for Eem object.  part of work for EEMS-139

--- app/models/eem.rb
+++ app/models/eem.rb
@@ -1,34 +1,13 @@
 require "active_fedora"
 require "lyber_core"
 require 'dor/action_log_datastream'
+require 'eem_model'
 
 
-class Eem < Dor::Base
-  include EemAccession
-  
-  has_relationship "parts", :is_part_of, :inbound => true #content files
-  
-  has_metadata :name => 'eemsProperties', :type => ActiveFedora::MetadataDatastream do |m|
-    m.label = "eemsProperties"
+class Eem < EemModel::Eem
+  include EemModel::EemAccession
+  include DorBase
     
-    m.field "copyrightStatusDate", :string, :multiple => false #mutiple doesn't do anything
-    m.field "copyrightStatus", :string
-    m.field "creatorOrg", :string
-    m.field "creatorPerson", :string
-    m.field "language", :string
-    m.field "note", :string
-    m.field "paymentType", :string
-    m.field "paymentFund", :string
-    m.field "selectorName", :string
-    m.field "selectorSunetid", :string
-    m.field "title", :string
-    m.field "sourceUrl", :string
-    m.field "requestDatetime", :string
-    m.field "status", :string
-    m.field "statusDatetime", :string
-    m.field "downloadDate", :string
-  end
-  
   has_metadata :name => 'actionLog', :type => Dor::ActionLogDatastream do |m|
     #nada
   end
