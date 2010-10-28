a80ab732369c6d5b9f979d2a7c52e7532a128c86
Willy Mene <wmene@stanford.edu>
2010-10-28 11:01:04 -0700

    Remove #find and #validate? methods.  EEMS-134

--- spec/lib/eems_user_spec.rb
+++ spec/lib/eems_user_spec.rb
@@ -1,31 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe EemsUser do
-  it "should generate a display name" do
-    u = EemsUser.new('first', 'last', 'sunetid')
+  it "generates a display name" do
+    u = EemsUser.new('first last', 'sunetid')
     u.display_name.should == 'first last'
   end
   
-  it "should generate a last, first display name" do
-    u = EemsUser.new('first', 'last', 'sunetid')
-    u.display_name_lf.should == 'last, first'
+  it "does not have a #find method" do
+    EemsUser.should_not respond_to(:find)
   end
   
-  describe "#find" do
-    it "should find users as defined in /config/users.yml" do
-      w = EemsUser.find('wmene')
-      w.sunetid.should == 'wmene'
-    end
+  it "does not have a #valid? method" do
+    EemsUser.should_not respond_to(:valid?)
   end
   
-  describe "#valid?" do
-    it "should return true if the passed in sunetid is a valid id" do
-      EemsUser.valid?('wmene').should be_true
-    end
-    
-    it "should return false if the passed in sunetid is not a valid id" do
-      EemsUser.valid?('joeschmoe').should_not be_true
-    end
-  end
-
 end
