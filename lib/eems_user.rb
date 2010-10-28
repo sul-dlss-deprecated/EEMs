f688689dfe120234a7c022f02b51ec69c501204f
Willy Mene <wmene@stanford.edu>
2010-10-28 11:01:04 -0700

    Remove #find and #validate? methods.  EEMS-134

--- lib/eems_user.rb
+++ lib/eems_user.rb
@@ -1,42 +1,10 @@
-require 'yaml'
-
 class EemsUser
-  @@all_users = nil
+
+  attr_reader :display_name, :sunetid
   
-  attr_reader :first_name, :last_name, :sunetid
-  
-  def initialize(fname, lname, sunetid)
-    @first_name = fname
-    @last_name = lname
+  def initialize(display_name, sunetid)
+    @display_name = display_name
     @sunetid = sunetid
   end
   
-  def display_name
-    @first_name + ' ' + @last_name
-  end
-  
-  def display_name_lf
-    @last_name + ', ' + @first_name
-  end
-    
-  def EemsUser.find(sunetid)
-    load_config
-    
-    u = @@all_users[sunetid]
-    return EemsUser.new(u['first_name'], u['last_name'], sunetid) if(u)
-    nil
-  end
-  
-  def EemsUser.valid?(sunetid)
-    load_config
-    
-    return @@all_users.member?(sunetid)
-  end
-  
-  protected
-  def EemsUser.load_config
-    unless(@@all_users)
-      @@all_users = YAML::load_file(File.join(RAILS_ROOT, "config", "users.yml"))
-    end
-  end
 end
