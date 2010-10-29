6cd501694609f9044408cc42e93e013e4c54b7d8
Willy Mene <wmene@stanford.edu>
2010-10-29 15:18:23 -0700

    EemsUser now stores privgroup EEMS-134

--- lib/eems_user.rb
+++ lib/eems_user.rb
@@ -1,20 +1,21 @@
 class EemsUser
 
-  attr_reader :display_name, :sunetid
+  attr_reader :display_name, :sunetid, :privgroup
   
-  def initialize(display_name, sunetid)
+  def initialize(display_name, sunetid, privgroup=nil)
     @display_name = display_name
     @sunetid = sunetid
+    @privgroup = privgroup
   end
   
   def save_to_session(session)
-    u = {:display_name => @display_name, :sunetid => @sunetid}
+    u = {:display_name => @display_name, :sunetid => @sunetid, :privgroup => @privgroup}
     session[:eems_user] = u
   end
   
   # TODO what to do when there is no :eems_user in the session
   def EemsUser.load_from_session(session)
-    u = EemsUser.new(session[:eems_user][:display_name], session[:eems_user][:sunetid])
+    u = EemsUser.new(session[:eems_user][:display_name], session[:eems_user][:sunetid], session[:eems_user][:privgroup])
     u
   end
   
