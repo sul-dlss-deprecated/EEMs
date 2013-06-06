378977bdf8411a92761cd4461fb614ece3b895f2
Willy Mene <wmene@stanford.edu>
2013-06-06 15:50:28 -0700

    Source /etc/rvmrc when stopping delayed job

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -54,7 +54,7 @@ after "deploy:rollback", "eems:start_delayed_job"
 
 namespace :eems do
   def run_dj_cmd(script)
-    cmd = "cd #{current_path}; bundle exec ./#{script} #{rails_env}"
+    cmd = "cd #{current_path}; source /etc/rvmrc; bundle exec ./#{script} #{rails_env}"
     run cmd
   end
 
