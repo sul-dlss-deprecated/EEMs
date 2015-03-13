9b2c4bca539603449db0734570f7d4bf1d5c8d6d
Willy Mene <wmene@stanford.edu>
2015-03-13 16:20:42 -0700

    Update paths for delayed_job scripts

--- stopjob.sh
+++ stopjob.sh
@@ -6,4 +6,4 @@ if [ -z $1 ]; then
 	exit
 fi
 
-/usr/bin/env RAILS_ENV=$1 /home/lyberadmin/eems/current/script/delayed_job stop
+/usr/bin/env RAILS_ENV=$1 /opt/app/eems/eems/current/script/delayed_job stop
